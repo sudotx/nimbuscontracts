b44e1bc505de71f32453102591c9a350765cdec6
0xfps <anthony.nnaemeka.umeh@gmail.com>
2026-03-02 10:59:11 +0100

    feat: `Market.sol`.
    This is a simple contract containing every data a market would need to be a prediction market.

---
+++ src/utils/Market.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+enum MarketState {
+    PRETRADE,   // Trading not yet active.
+    OPEN,       // Trading active.
+    CLOSED,     // Trading ended, awaiting resolution.
+    RESOLVED,   // Outcome determined.
+    INVALID     // Market invalidated.
+}
+
+enum MarketType {
+    BINARY,         // YES/NO.
+    CATEGORICAL,    // Multiple outcomes.
+    SCALAR,         // Range-based.
+    CONDITIONAL     // Depends on another market.
+}
+
+struct MarketInfo {
+    MarketState marketState;
+    MarketType marketType;
+    address creator;
+    address resolver;
+    string question;
+    string description;
+    uint16 category;
+    uint16 subcategory;
+    uint64 createdAt;
+    uint64 endTime;
+    uint64 resolvedAt;
+}
